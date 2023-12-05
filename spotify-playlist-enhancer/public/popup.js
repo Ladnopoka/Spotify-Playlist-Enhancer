ac7253c9dbaaf7e79221cc1d21a41b7cb1f221f1
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-12-05 19:21:57 +0000

    The chrome extension now has 3 spotify themed buttons

--- spotify-playlist-enhancer/public/popup.js
+++ spotify-playlist-enhancer/public/popup.js
@@ -1,3 +1,13 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({ url: 'https://example.com' });
+import './popup.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('myButton').addEventListener('click', function () {
+    chrome.tabs.create({ url: 'localhost:3000' });
+  });
+  document.getElementById('myButton2').addEventListener('click', function () {
+    // Code for "My Playlists" button
+  });
+  document.getElementById('myButton3').addEventListener('click', function () {
+    // Code for "Recent Playlists" button
+  });
 });
