b2128fc833188e0568719c4d7a6698ea01274297
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-12-05 22:52:54 +0000

    The extension now has a nice player and list of songs to select

--- spotify-playlist-enhancer/public/popup.js
+++ spotify-playlist-enhancer/public/popup.js
@@ -1,21 +1,32 @@
 //import './popup.css';
 
 document.addEventListener('DOMContentLoaded', function () {
+
+
   document.getElementById('myButton').addEventListener('click', function () {
     chrome.tabs.create({ url: 'login.html' });
   });
+
+
   document.getElementById('myButton2').addEventListener('click', function () {
     var musicPlayerPopup = document.getElementById('musicPlayerPopup');
     if(musicPlayerPopup.style.display === 'none') {
         musicPlayerPopup.style.display = 'block';
         // Load and list music files here, setting them as the source for the audio player
+        addSongToPopup('songs/MilkyChanceStolenDance.mp3')
+        addSongToPopup('songs/frozen.mp3')
+        addSongToPopup('songs/after_dark.mp3')
     } else {
         musicPlayerPopup.style.display = 'none';
     }
   });
+
+
   document.getElementById('myButton3').addEventListener('click', function () {
     // Code for "Recent Playlists" button
   });
+
+
 });
 
 // Function to add songs to the popup
