23a2767a4a2d24ccc45cdf716bce5701189a4c42
Fiachra101 <fiachra101@gmail.com>
2023-12-19 13:24:20 +0000

    2 buttons and placeholder boxes for songs

--- spotify-playlist-enhancer/spotify-chrome-extension/src/App.js
+++ spotify-playlist-enhancer/spotify-chrome-extension/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+<link rel="stylesheet" type="text/css" href="spotify-api/spotify-playlist-enhancer/src/App.css"></link>
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fabian is a gobshite.
+          
         </p>
         <a
           className="App-link"
@@ -16,10 +17,40 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+  </a>*/}
+      
+        <button class="createButton" onClick={() => alert('Create Playlist button clicked')}>
+          Create Playlist
+        </button>
+        <button class="viewButton" onClick={() => alert('Create Playlist button clicked')}>
+          View Playlists
+        </button>
+        
+        <div class="squareL"></div>
+        <div class="squareR"></div>
+
+        <div class="icon">
+        <div class="arrow"></div>
+        </div>
+        
+
+
       </header>
     </div>
   );
 }
+//Makes arrow move back and forth on click
+const $icon = document.querySelector('.icon');
+const $arrow = document.querySelector('.arrow');
 
+$icon.onclick = () => {
+  $arrow.animate([
+    {left: '0'},
+    {left: '10px'},
+    {left: '0'}
+  ],{
+    duration: 700,
+    iterations: Infinity
+  });
+}
 export default App;
