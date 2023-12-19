fbc1f08b876f8e7b46e1e45d34a64c99eaaf3c0b
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-12-19 18:06:49 +0000

    working on background.js code

--- spotify-playlist-enhancer/public/background.js
+++ spotify-playlist-enhancer/public/background.js
@@ -1,4 +1,25 @@
-chrome.action.onClicked.addListener((tab) => {
-    console.log("Chrome extension happened yep...")
-    chrome.tabs.create({ url: 'https://example.com' });
-  });
+// chrome.action.onClicked.addListener((tab) => {
+//     console.log("Chrome extension happened yep...")
+//     chrome.tabs.create({ url: 'https://example.com' });
+//   });
+
+let user_signed_in = false;
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'login') {
+    if (user_signed_in)
+    {
+      console.log("User is already signed in.");
+    } else {
+      // sign in to spotify here
+    }
+  } else if (request.message === 'logout') {
+    user_signed_in = false;
+    chrome.browserAction.setPopup({ popup: './popup.html' }, () => {
+      sendResponse({ message: 'success' });
+    });
+
+    return true;
+  }
+});
+  
